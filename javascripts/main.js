7f96c479810f711fcc92e4d053fa377993c3b15c
Kingpixel <kingpixel@gmail.com>
2020-05-07 18:14:04 -0700

    Adds site navigation

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,16 @@
-console.log('This would be the main JS file.');
+
+(function () {
+    // get the button and menu nodes
+    var button = document.querySelector('[aria-label="site"] button');
+    var menu = button.nextElementSibling;
+    // set initial (closed menu) states
+    button.setAttribute('aria-expanded', 'false');
+    menu.hidden = true;
+    button.addEventListener('click', function () {
+        // toggle menu visibility
+        var expanded = this.getAttribute('aria-expanded') === 'true';
+        this.setAttribute('aria-expanded', String(!expanded));
+        menu.hidden = expanded;
+    });
+
+})()
